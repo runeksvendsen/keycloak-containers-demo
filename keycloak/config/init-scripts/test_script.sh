ec9bca6751f53e2a3d5913617cd6165ba3e5ed90
Rune K. Svendsen <runesvend@gmail.com>
2021-02-09 14:42:09 +0100

    test_script.sh: fix host

--- keycloak/config/init-scripts/test_script.sh
+++ keycloak/config/init-scripts/test_script.sh
@@ -2,7 +2,7 @@
 
 echo "Hello from 'test_script.sh'"
 
-kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password admin
+kcadm.sh config credentials --server http://keycloak:8080/auth --realm master --user admin --password admin
 kcadm.sh get realms/master > masterrealm.json
 cat masterrealm.json
 
