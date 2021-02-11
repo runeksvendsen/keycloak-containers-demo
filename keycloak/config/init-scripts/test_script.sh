3ee8af1ff8da586bbe4530fa65fc978128c663a5
Rune K. Svendsen <runesvend@gmail.com>
2021-02-11 15:58:43 +0100

    keycloak secret key + postgrest public key

--- keycloak/config/init-scripts/test_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Hello from 'test_script.sh'"
-
-kcadm.sh config credentials --server http://keycloak:8080/auth --realm master --user admin --password admin
-kcadm.sh get realms/master > masterrealm.json
-cat masterrealm.json
-
-echo "'test_script.sh': Done!"
